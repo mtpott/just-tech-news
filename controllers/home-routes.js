6d763a004c57ab1474a2357ca992594b2c0ab047
megan <mtpott23@gmail.com>
2022-04-25 10:11:18 -0400

    handlebars homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,55 @@
+//contains all of the user-facing routes, such as homepage and login page
+
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        //pass a single post object into the homepage template
+        //this will loop over and map each Sequelize object into a serialized version of itself, saving the results in a new 'posts' array. we can use that array in the template, but even though the render() method can accept an array instead of an object, that will prevent us from adding other properties to the template later on. instead, add the array to an object and continue passing an object to the template.
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+    
+    // res.render('homepage', {
+    //     id: 1,
+    //     post_url: 'https://handlebarsjs.com/guide/',
+    //     title: 'Handlebars Docs',
+    //     created_at: new Date(),
+    //     vote_count: 10,
+    //     comments: [{}, {}],
+    //     user: {
+    //         username: 'test_user'
+    //     }
+    // });
+});
+
+module.exports = router;
